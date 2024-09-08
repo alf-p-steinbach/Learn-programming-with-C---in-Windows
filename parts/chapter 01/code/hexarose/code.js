af8fe9b27e047c360bd52dbf8c841843e3ac0e0a
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-09-08 07:51:19 +0200

    -

---
+++ parts/chapter 01/code/hexarose/code.js
@@ -0,0 +1,67 @@
+﻿"use strict"
+function $( selector )  { return document.querySelector( selector ); }
+function invoke( f )    { return f(); }
+
+class Position
+{
+    x = 0;  y = 0;
+    constructor( _x = 0, _y = 0 )   { this.x = _x; this.y = _y; }
+    plus( other )                   { return new Position( this.x + other.x, this.y + other.y ); }
+    scaledBy( c )                   { return new Position( c*this.x, c*this.y ); }
+}
+
+const canvasElement     = $( 'canvas' );        // Assumes there is a single `<canvas>` element.
+const width             = canvasElement.width;
+const height            = canvasElement.height;
+const canvasSurface     = canvasElement.getContext( '2d' );
+
+const n_points      = 6;
+const fullCircle    = 2*Math.PI;                // 360 degrees.
+const sixth         = fullCircle/n_points;      // 60 degrees.
+const center        = new Position( width/2, height/2 );
+const radius        = Math.min( center.x, center.y );
+const points        = invoke( () =>
+{
+    let result = new Array( n_points );
+    for( let i = 0; i < n_points; ++i ) {
+        const endPoint = new Position( Math.cos( i*sixth ), Math.sin( i*sixth ) );
+        result[i] = endPoint.scaledBy( radius );
+    }
+    return result;
+} );
+
+function draw()
+{
+    const s = canvasSurface;
+    s.lineWidth = 7;  s.lineCap = 'round';
+
+    s.strokeStyle = 'black';  s.fillStyle = 'black';
+    {
+        const p = new Path2D();
+        p.rect( 0, 0, width, height );
+        s.fill( p );
+        s.stroke( p );
+    }
+    
+    s.strokeStyle = 'red';  s.fillStyle = 'green';
+    {
+        const p = new Path2D();
+        p.arc( center.x, center.y, radius, 0, fullCircle );
+        s.fill( p );
+        s.stroke( p );
+    }
+
+    s.strokeStyle = 'red';  s.fillStyle = 'white';
+    for( let i = 0; i < n_points; ++i ) {
+        const p = new Path2D();
+        const o1 = points[i].plus( center );
+        p.arc( o1.x, o1.y, radius, (2 + i)*sixth, (3 + i)*sixth );
+        const j = (i + 2) % n_points;
+        const o2 = points[j].plus( center );
+        p.arc( o2.x, o2.y, radius, (5 + i)*sixth, (6 + i)*sixth );
+        s.fill( p );
+        s.stroke( p );
+    }
+}
+
+draw();
---
+++ parts/chapter 01/code/prime-numbers.cpp
@@ -0,0 +1,50 @@
+﻿// Prime numbers up to some max value, using O(N) memory where N is number of primes.
+// A sort of /dynamic/ sieve (see “Sieve of Eratosthenes” for static sieve approach).
+#include <iostream>
+#include <queue>
+#include <vector>
+
+auto& out = std::cout;
+
+template< class Item, class Compare_func >
+using Priority_q = std::priority_queue<Item, std::vector<Item>, Compare_func>;
+
+struct Multiple
+{
+    int base; int n;
+    auto value() const -> int{ return n*base; }
+};
+    
+struct Descending_order
+{
+    auto operator()( const Multiple& a, const Multiple& b ) const
+        -> bool
+    { return a.value() > b.value(); }       // Note: direction of comparison.
+};
+
+auto main() -> int
+{
+    const int max_value = 1'000;        // Or a billion. Or a hundred. Whatever.
+    out << 2;
+    auto larger_composites = Priority_q<Multiple, Descending_order>();
+    larger_composites.push( Multiple{2, 2} );
+    for( int current = 3; current < max_value; ++current ) {
+        bool is_composite = false;
+        for( ;; ) {
+            const Multiple next_composite = larger_composites.top();
+            if( current < next_composite.value() ) {
+                break;
+            }
+            larger_composites.pop();
+            is_composite = true;
+            larger_composites.push( Multiple{next_composite.base, 1 + next_composite.n} );
+        }
+        if( not is_composite ) {
+            out << ' ' << current;
+            larger_composites.push( Multiple{current, 2} );
+        }
+    }
+    out << '\n';
+    out << "Found " << larger_composites.size() << " primes less than " << max_value << ".\n";
+}
+
